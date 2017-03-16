f3bd497456eb966aba1e0951105feb10c7fceca1
OlegMikhailov23 <Dkfolkin@Mikhailovs-MacBook-Air.local>
2017-03-16 14:30:51 +0300

    dfg

--- order.php
+++ order.php
@@ -12,16 +12,21 @@
    $back = "<p><a href=\"javascript: history.back()\">Вернуться назад</a></p>";
  
    if(!empty($_POST['name']) and !empty($_POST['phone']) and 
-   !empty($_POST['message'])){
+   !empty($_POST['select'])){
       $name = trim(strip_tags($_POST['name']));
       $phone = trim(strip_tags($_POST['phone']));
-      $message = trim(strip_tags($_POST['message']));
+      $select = trim(strip_tags($_POST['select']));
+      $comment-to-order = trim(strip_tags($_POST['comment-to-order']));
  
-      mail('dkfolkin@gmail.com', 'Письмо с EDCSTORE', 
-      'Вам написал: '.$name.'<br />Его номер: '.$phone.'<br />
-      Его сообщение: '.$message,"charset=utf-8");
+      mail('dkfolkin@gmail.com',
+           'ЗАКАЗ с EDCSTORE', 
+      'Вам написал: '.$name.'<br />
+      Его номер: '.$phone.'<br />
+      Его комментарий к заказу: '.$comment-to-order.'<br />
+      Его сообщение: '.$message,
+           "charset=utf-8");
  
-      echo "Ваше сообщение успешно отправлено!<Br> Вы получите ответ в 
+      echo "Ваш заказ успешно отправлен!<Br> Вы получите ответ в 
       ближайшее время<Br> $back";
  
       exit;
