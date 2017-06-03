12693b43fdb4126e844424381dc82d79c5ab386a
OlegMikhailov23 <Dkfolkin@Mikhailovs-MacBook-Air.local>
2017-06-03 20:05:26 +0300

    install recaptcha2

--- mail.php
+++ mail.php
@@ -1,84 +1,23 @@
-<!DOCTYPE html>
-<html lang="ru">
-
-<head>
-    <meta charset="utf-8">
-    <title>EDC обратная связь</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700&amp;subset=latin,cyrillic">
-    <link rel="stylesheet" href="CSS/mini-style.css">
-</head>
-
-<body>
-    <header class="main-header">
-        <div class="container clearfix">
-            <nav class="main-navigation">
-                <ul>
-                    <li>
-                        <a href="index.html"><img src="img/logo-site.png" width="190" height="30" alt="EDC STORE"></a>
-                    </li>
-                    <li>
-                        <a href="goods.html">Товары</a>
-                    </li>
-                    <li>
-                        <a href="Order-form.html">Заказать</a>
-                    </li>
-                    <li>
-                        <a href="news.html">Новости</a>
-                    </li>
-                    <li>
-                        <a href="about-us.html">О нас</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-    <main class="container">
-        <div class="form-echo-container">
-            <h2>☺Ваш запрос отправлен☺</h2>
-            <h2>Мы с Вами свяжемся в ближайшее время!</h2>
-            <a href="goods.html" class="btn">Назад к покупкам!</a>
-        </div>
-    </main>
-    <footer class="main-footer">
-        <div class="container clearfix">
-            <section class="footer-contacts"><span class="footer-signature">
-          #E.D.C. Store</span>
-                <br>
-                <br> Телефон: +7 (911) 731-84-25
-            </section>
-            <section class="footer-social">
-                <p>Давайте дружить!</p>
-                <a class="social-btn social-btn-vk" href="https://vk.com/e_d_c_store" target="_blank">Вконтакте</a>
-                <a class="social-btn social-btn-inst" href="https://www.instagram.com/edc_store_kydex_holster/" target="_blank">Инстаграм</a>
-            </section>
-            <section class="footer-copyright">
-                <p>Разработано:</p>
-                <a class="btn" href="https://vk.com/huzky_dj" target="_blank">Oleg Mikhailov</a>
-            </section>
-        </div>
-    </footer>
-    <?php
-   
- 
-   if(!empty($_POST['name']) and !empty($_POST['phone']) and 
-   !empty($_POST['message'])){
+<?php
+  if (isset($_POST['g-recaptcha-response']) && $_POST['g-recaptcha-response']) {
+    $secret = '6LeOJiMUAAAAAOMJk1Vy5vWCYX0m1YjPvty6GBwq';
+    $ip = $_SERVER[REMOTE_ADDR];
+    $response = $_POST['g-recaptcha-response'];
+    $rsp = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$response&remoteip=$ip");
+    $arr = json_decode($rsp, TRUE);
+  }
+  if(!empty($_POST['name']) and !empty($_POST['phone']) and 
+   !empty($_POST['message']) and $arr['success']){
       $name = trim(strip_tags($_POST['name']));
       $phone = trim(strip_tags($_POST['phone']));
       $message = trim(strip_tags($_POST['message']));
- 
-      mail('dkfolkin@gmail.com', 'Письмо с EDCSTORE', 
-      'Вам написал: '.$name.'<br />Его номер: '.$phone.'<br />
-      Его сообщение: '.$message,"charset=utf-8");
- 
-      
- 
+      mail('dkfolkin@gmail.com','Письмо с EDCSTORE','Вам написал: '.$name.'<br>Его номер: '.$phone.'<br>Его сообщение: '.$message,"charset=utf-8");
+      header("Location: send-form-ok.html");
       exit;
    } 
    else {
-      echo "Для отправки сообщения заполните все поля! $back";
+      header("Location: error.html");
+      echo "Заполните все поля!";
       exit;
    }
 ?>
-</body>
-
-</html>
